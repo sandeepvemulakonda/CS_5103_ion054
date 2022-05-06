6ce8766d5981eba01379b13ec02eba40de2e712a
Saisandeep <sandeepvemulakonda96@gmail.com>
2022-05-06 10:45:36 -0500

    Third  Requirement added

--- sandeep.py
+++ sandeep.py
@@ -1,40 +1,54 @@
 import re
-#function for unique word count
+
+
+# function for unique word count
 def uniqueWordCout(fileContent):
     uniqueWordDictionary = {}
     words = filter(None, re.split(' |\n|\t', fileContent))
     for word in words:
         uniqueWordDictionary[word] = uniqueWordDictionary.get(word, 0) + 1
     return uniqueWordDictionary
+
+
 # function for replacement
-   
 def replaceWord(fileContent, findBy, replaceBy):
-    return re.sub('[ \t\n]'+findBy+'[ \t\n]', lambda x : x.group().replace(findBy, replaceBy), fileContent)
+    return re.sub('[ \t\n]' + findBy + '[ \t\n]', lambda x: x.group().replace(findBy, replaceBy), fileContent)
 
-#replace file structure with the ones in your system
 
-inputFile = (open('C:/Users/Sandeep pc/Desktop/SE_Project/sample.txt'))
+# function to print all lines that contains keyword as full word
+def grepline(fileContent, keyWord):
+    lines = fileContent.split('\n')
+    print("keyword line")
+    for line in lines:
+        if (bool(re.search('[ \t\n]' + keyWord + '[ \t\n]', line + "\n"))):
+            print(line)
+
+
+
+# replace file structure with the ones in your system
+
+inputFile = (open("sample.txt"))
 a = inputFile.read()
 
 b = uniqueWordCout(a)
 
-#print(b)
+# print(b)
 
 print(" \n Frequency of each word: \n")
 for key in b:
-    
-    print("\t",key, '->', b[key])
+    print("\t", key, '->', b[key])
 
 print("\n")
 
 print("\n")
-print("Original text message= ",a)
+print("Original text message= ", a)
 print("\n")
-
-
 
 existing_word = input("Enter the existing_word= ")
 replace_word = input("\nEnter the replacing_word = ")
 print("\n")
 
-print("After replacing text message =",replaceWord(a,existing_word ,replace_word))
+print("After replacing text message =", replaceWord(a, existing_word, replace_word))
+keyword = input("enter the keyword = ")
+grepline(a,keyword)
+
